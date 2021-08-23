671d332f931a501bc69c4c006a4a4ce4f0731603
Daniel Herskowitz <4458323+dherskowitz@users.noreply.github.com>
2021-08-23 12:06:49 +0300

    change port

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,15 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Tal Chaim',
-  plugins: []
-}
+    siteName: "Tal Chaim",
+    port: 3000,
+    plugins: [
+        {
+            use: "gridsome-plugin-tailwindcss",
+        },
+    ],
+};
